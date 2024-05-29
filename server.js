a6a69bb45abbc96dd1786915574968aa4b1eabd1
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-05-29 15:54:07 +0300

    db connection and middlewares

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import {fileURLTopath} from 'url'
 dotenv.config()
 
-const PORT = pgrocess.env.PORT
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+
+const PORT = process.env.PORT
+const __filename = fileURLTopath(import.meta.utl)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
 app.listen(PORT, () => {
     console.log('Server is running')
+})
+
+//database connection
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to Database')
+}).catch((err) => {
+    console.log(err)
+})
+
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || '500'
+    const message = err.message || 'Internal server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
 })
 
